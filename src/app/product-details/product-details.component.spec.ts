bb66d7b2fae74e9ed72378f61e93bc34de64dbdc
jmathie9 <jmathie9@ford.com>
2023-04-03 12:07:32 -0400

    Configure Karma; add first unit test!

--- src/app/product-details/product-details.component.spec.ts
+++ src/app/product-details/product-details.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { productsStub } from 'src/test/products-stub';
 import { ProductDetailsComponent } from './product-details.component';
 
 describe('ProductDetailsComponent', () => {
@@ -8,9 +10,12 @@ describe('ProductDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductDetailsComponent ]
+      declarations: [ProductDetailsComponent],
+      providers: [
+        provideRouter([]),
+      ],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
@@ -20,4 +25,18 @@ describe('ProductDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display product name, price and description', () => {
+    component.product = productsStub[0];
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+    const productName = fixture.debugElement.query(By.css('h3'));
+    const productPrice = fixture.debugElement.query(By.css('h4'));
+    const productDescription = fixture.debugElement.query(By.css('p'));
+    expect(productName.nativeElement.innerText).toBe(productsStub[0].name);
+    expect(productPrice.nativeElement.innerText).toBe('$' + productsStub[0].price + '.00');
+    expect(productDescription.nativeElement.innerText).toBe(productsStub[0].description);
+  });
 });
+
+
---
+++ src/test/products-stub.ts
@@ -0,0 +1,8 @@
+export const productsStub = [
+  {
+    id: 1,
+    name: 'Test Phone',
+    price: 799,
+    description: 'A test phone that works great in test scenarios!'
+  }
+];
